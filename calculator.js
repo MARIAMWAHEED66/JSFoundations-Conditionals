0a00ec8c8f04b5411a98c3f32719c86ea350ce2f
Sabreen Bucheerei <m_7maid@hotmail.com>
2021-01-10 15:56:37 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,30 @@
 // Define the numbers and the operation here
 
 // Write if-statements here
+
+// Use this code in repl.it to get the user's input
+// const user = prompt("Please enter name")
+
+// console.log(user)
+
+num1= 100;
+num2= 50;
+op= "/";
+
+ if(op="/"){
+    console.log(`${num1} / ${num2} = ${num1/num2}`); 
+   }
+       
+  else if (op="-"){ 
+    console.log(`${num1} - ${num2} = ${num1-num2}`);  
+}
+    else if (op="*"){
+        console.log(`${num1} * ${num2} = ${num1*num2}`);  
+   }
+   else if (op="+"){
+    console.log(`${num1} + ${num2} = ${num1+num2}`); 
+   }
+
+ else {
+     console.log("operation is undefiend");
+  }
